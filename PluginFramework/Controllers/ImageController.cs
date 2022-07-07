6e7440dab51c02cc36e397041eb84c5a0b2a73e6
bhagy <bhagy@VENUS>
2022-07-08 01:12:03 +0530

    Add project files.

---
+++ PluginFramework/Controllers/ImageController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PluginFramework.Models;
+
+namespace PluginFramework.Controllers
+{
+    public class ImageController : Controller
+    {
+        private readonly ImageDbContext _context;
+
+        public List<Image> Images = new List<Image>();
+        public ImageController(ImageDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            
+            Images = _context.Images.ToList();
+            return View(Images);
+        }
+
+        public IActionResult OnGetMyOnClick()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
---
+++ PluginFramework/Models/Image.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PluginFramework.Models
+{
+    public class Image
+    {
+        public int id { get; set; }
+
+        public string img { get; set; }
+
+    }
+
+    public class ImageDbContext: DbContext
+    {
+        public ImageDbContext(DbContextOptions<ImageDbContext>options):base(options)
+        { }
+        public DbSet<Image> Images { get; set; }
+    }
+}
